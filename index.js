e99ed5c0e67b67f05307d4ddd3006c185b2fb1f0
dedzec <dedzec@gmail.com>
2020-04-17 02:07:10 -0300

    feat: add Watson natural languagem understanting service

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const readline = require('readline-sync');
 const robots = {
-    text: require('./robots/text.js')
+    text: require('./robots/text.js'),
 };
 
 async function start() {
-    const content = {};
+    const content = {
+        maximumSentences: 7,
+    };
 
     content.searchTerm = askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix();
@@ -23,7 +25,7 @@ async function start() {
         return selectPrefixText;
     }
 
-    console.log(content);
+    console.log(JSON.stringify(content, null, 4));
 }
 
 start();
